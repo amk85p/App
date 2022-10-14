a7fd19ee14566012ce14198da78caea7f93005ba
Anna Kalinowska <mirska.anna@gmail.com>
2022-10-14 18:34:31 +0200

    cardForm

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,35 +1,81 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
-  return (
-    <div className={styles.list}>
-      <header className={styles.header}>
-        <h2 className={styles.title}>
-          Things to do<span>soon</span>
-        </h2>
-      </header>
-      <p className={styles.description}>
-        Interesting things I want to check out!
-      </p>
-      <section className={styles.columns}>
-        <Column title='Books' icon='book' />
-        <Column title='Movies' icon='film' />
-        <Column title='Games' icon='gamepad' />
-      </section>
-      {/* <section className={styles.columns}>
-        <article>
-          <h2>Books</h2>
-        </article>
-        <article>
-          <h2>Movies</h2>
-        </article>
-        <article>
-          <h2>Games</h2>
-        </article>
-      </section> */}
-    </div>
-  );
+  const [columns, setColumns] = useState([
+		{
+			id: 1,
+			title: 'Books',
+			icon: 'book',
+			cards: [
+				{ id: 1, title: 'This is Going to Hurt' },
+				{ id: 2, title: 'Interpreter of Maladies' }
+			]
+		},
+		{
+			id: 2,
+			title: 'Movies',
+			icon: 'film',
+			cards: [
+				{ id: 1, title: 'Harry Potter' },
+				{ id: 2, title: 'Star Wars' }
+			]
+		},
+		{
+			id: 3,
+			title: 'Games',
+			icon: 'gamepad',
+			cards: [
+				{ id: 1, title: 'The Witcher' },
+				{ id: 2, title: 'Skyrim' }
+			]
+		}
+	]);
+
+	const addColumn = newColumn => {
+		console.log('addColumn ',addColumn  )
+    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
+};
+
+const addCard = (newCard, columnId) => {
+	const columnsUpdated = columns.map(column => {
+		if(column.id === columnId)
+			return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
+		else
+		
+			return column
+	})
+
+	setColumns(columnsUpdated);
+
+};
+
+	return (
+		<div className={styles.list}>
+			
+			<header className={styles.header}>
+				
+				<h2 className={styles.title}>Things to do<span>soon!</span></h2>
+			
+			</header>
+			
+			<p className={styles.description}>Interesting things I want to check out</p>
+			
+			<section className={styles.columns} >
+			
+			{columns.map(column => 
+									<Column key={column.id} id={column.id} title={column.title} icon={column.icon} 
+									cards={column.cards} action={addCard} />)}
+			
+			</section>
+			
+			<ColumnForm action={addColumn} />			
+			
+		</div>
+	);
 };
 
 export default List;
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,6 +3,7 @@ import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
 
 const SearchForm = () => {
+  
   return (
     <form className={styles.searchForm}>
       <TextInput placeholder='Search...' />
