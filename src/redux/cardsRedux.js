887712cf85e116a17b813dc27c22f006dbb1568e
Anna Kalinowska <mirska.anna@gmail.com>
2022-11-15 17:01:36 +0100

    fixes

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,6 +1,6 @@
 import shortid from 'shortid';
 import strContains from '../utils/strContains';
-
+// selectors
 export const getFavoriteCards = ({ cards }) =>
   cards.filter((card) => card.isFavorite === true);
 
@@ -10,32 +10,36 @@ export const getFilteredCards = ({ cards, searchString }, columnId) =>
       card.columnId === columnId && strContains(card.title, searchString)
   );
 
+// actions
 const createActionName = (actionName) => `app/cards/${actionName}`;
 const ADD_CARD = createActionName('ADD_CARD');
+const TOGGLE_CARD_FAVORITE = createActionName('TOGGLE_CARD_FAVORITE');
+const REMOVE_CARD = createActionName('REMOVE_CARD');
 
-export const addCard = (payload) => ({ type: 'ADD_CARD', payload });
+// action creators
+export const addCard = (payload) => ({ type: ADD_CARD, payload });
 
 export const toggleCardFavorite = (payload) => ({
-  type: 'TOGGLE_CARD_FAVORITE',
+  type: TOGGLE_CARD_FAVORITE,
   payload,
 });
 
 export const removeCard = (payload) => ({
-  type: 'REMOVE_CARD',
+  type: REMOVE_CARD,
   payload,
 });
 
 const cardsReducer = (statePart = [], action) => {
   switch (action.type) {
-    case 'ADD_CARD':
+    case ADD_CARD:
       return [...statePart, { ...action.payload, id: shortid() }];
-    case 'TOGGLE_CARD_FAVORITE':
+    case TOGGLE_CARD_FAVORITE:
       return statePart.map((card) =>
         card.id === action.payload
           ? { ...card, isFavorite: !card.isFavorite }
           : card
       );
-    case 'REMOVE_CARD':
+    case REMOVE_CARD:
       return statePart.filter((card) => card.id !== action.payload);
 
     default:
--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,14 +1,15 @@
+// actions
 const createActionName = (actionName) => `app/searchString/${actionName}`;
 const UPDATE_SEARCHSTRING = createActionName('UPDATE_SEARCHSTRING');
-
+// action creators
 export const updateSearchstring = (payload) => ({
-  type: 'UPDATE_SEARCHSTRING',
+  type: UPDATE_SEARCHSTRING,
   payload,
 });
 
 const searchStringReducer = (statePart = '', action) => {
   switch (action.type) {
-    case 'UPDATE_SEARCHSTRING':
+    case UPDATE_SEARCHSTRING:
       return action.payload;
     default:
       return statePart;
